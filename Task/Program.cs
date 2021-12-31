64cc87396fa24b93b460afb548b190c380e14404
Sergey <sniper411@mail.ru>
2021-12-31 15:57:24 +0300

    added metod Print and metod Create

---
+++ Task/Program.cs
@@ -0,0 +1,24 @@
+﻿//Написать программу, которая из имеющегося массива целых чисел формирует массив из четных чисел. 
+
+int[] CreateArray(int Length, int LeftBound, int RigthBound)
+{
+    int[] Array = new int[Length];
+    for (int i = 0; i < Array.Length; i++)
+    {
+        Array[i] = new Random().Next(LeftBound, RigthBound);
+    }
+    return Array;
+}
+
+void PrintArray(int[] Array)
+{
+    for (int i = 0; i < Array.Length; i++)
+    {
+        System.Console.WriteLine(Array[i]);
+    }
+}
+
+int[] Arr = CreateArray(5, -10, 15);
+PrintArray(Arr);
+
+
